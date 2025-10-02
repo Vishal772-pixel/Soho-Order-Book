87fe51f31dcef2cba5a7165b533bf4c07694bc20
Vishal772-pixel <fantasydark001@gmail.com>
2025-10-02 14:40:05 +0530

    Create Order Completed

--- soho-contract/src/Order book/OrderBook.sol
+++ soho-contract/src/Order book/OrderBook.sol
@@ -41,9 +41,128 @@ contract Orderbook is {
         OrderStatus status;
     }
 
-    // States
+
+    /// -----------------------------
+    /// State
+    /// -----------------------------
     Ivault public immutable vault;
-    
+    address public owner;
+    uint256 public feeBps;
 
 
+   
+    /// -----------------------------
+    /// Order Storage
+    /// -----------------------------
+    uint256 public nextOrderId=1;
+    mapping (uint256=>Order) public orders;
+
+
+ 
+    /// -----------------------------
+    /// Constructor
+    /// -----------------------------
+
+constructor(address _vault , uint256 _feeBps){
+    if(_vault==address(0),OrderBook_ZeroVaultOrderBook());
+    if(_feeBps>1000,OrderBook_FeeTooHigh());
+    vault=Ivault(_vault);
+    msg.sender(owner);
+    feeBps=_feeBps;
+}
+
+
+
+
+   
+    /// -----------------------------
+    /// Modifier
+    /// -----------------------------
+modifier onlyOwner(){
+    if(msg.sender!=owner,OrderBook_OnlyOwner());
+    _;
+}
+
+
+   //Admin
+function setFeeBps(uint256 _b)external onlyOwner{
+    if(_b>1000,FeeBpsOnly10Percent());
+    feeBps=_b;
+}
+
+function transferOwnerShip(address _new) onlyOwner[
+    if(_new==address(0),OrderBook_ZeroAddress());
+    owner=_new;
+
+]
+
+
+
+ /// -----------------------------
+    /// Order lifecycle
+    /// -----------------------------
+
+
+
+ /**
+     * @notice Create an order on-chain. The user must already have deposited tokenIn into Vault.
+     * @param tokenIn Token the owner is selling.
+     * @param tokenOut Token the owner wants to receive.
+     * @param amountIn Amount of tokenIn owner is selling.
+     * @param amountOut Amount of tokenOut owner expects.
+     * @param expiry Timestamp after which order is invalid.
+     * @return orderId newly created order id
+     */
+   function createOrder(
+    address tokenIn,
+    address tokenOut,
+    uint256 amountIn,
+    uint256 amountOut,
+    uint256 expiry
+   ) external return(uint256 orderId){
+if(amountIn<=0&&amountOut<=0,OrderBook_ZeroAmount());
+if(tokenIn==tokenOutc,SameToken());
+if(expiry!=0||expiry<=block.timestamp,OrderBook_InvalidExpiry());
+
+
+// Ensuring user has deposited enough in Vault
+uint256 bal=vault.balances(msg.sender, tokenIn);
+ if(bal==amountIn,OrderBook_InsufficientAmount());
+
+
+ orderId=nextOrder++;
+ orders[orderId]=Order({
+    owner=msg.sender;
+    tokenIn=tokenIn;
+    tokenOut=tokenOut;
+    amountIn=amountIn;
+    amountOut=amountOut;
+    remainingIn=amountIn;
+    remainingOut=amountOut;
+    expiry=expiry;
+    status = Orderstatus.Active;
+ });
+
+ emit OrderCreated(orderId, msg.sender, tokenIn,tokenOut,amountIn,amountOut,expiry);
+
+   };
+
+
+
+
+    /**
+     * @notice Cancel an active order (owner only). Remaining funds become withdrawable from Vault.
+     * @param orderId id of order to cancel
+     */
+  
+
+
+
+
+
+
+
+
+
+   function cancelOrder() 
 }
